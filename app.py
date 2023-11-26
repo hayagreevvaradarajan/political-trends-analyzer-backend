0b4050658df75063e3c28638bb4dc5a186746606
Hayagreev Varadarajan <hayagreev.chintu1@gmail.com>
2023-11-26 15:36:40 -0500

    removed unnecessary methods

--- app.py
+++ app.py
@@ -297,50 +297,5 @@ ORDER BY
     }
     return jsonify(output_dict)
 
-@app.route('/query2', methods=['GET'])
-def query2():
-    params = oracledb.ConnectParams(host="oracle.cise.ufl.edu", port=1521, service_name="orcl")
-    conn = oracledb.connect(user="v.vadlamani", password="XEfjppuxN8M49BF8ccGDvnPf", params=params)
-    cursor = conn.cursor()
-    request_param = request.args
-    cursor.execute('select * from borders')
-    while True:
-        row = cursor.fetchone()
-        if row is None:
-            break
-        print(row)
-    conn.close()
-    # return jsonify({"message": "Connection successful, Query2"})
-    return request_param
-
-@app.route('/query3', methods=['GET'])
-def query3():
-    params = oracledb.ConnectParams(host="oracle.cise.ufl.edu", port=1521, service_name="orcl")
-    conn = oracledb.connect(user="v.vadlamani", password="XEfjppuxN8M49BF8ccGDvnPf", params=params)
-    cursor = conn.cursor()
-    cursor.execute('select * from city')
-    while True:
-        row = cursor.fetchone()
-        if row is None:
-            break
-        print(row)
-    conn.close()
-    return jsonify({"message": "Connection successful, Query3"})
-
-@app.route('/query5', methods=['GET'])
-def query5():
-    params = oracledb.ConnectParams(host="oracle.cise.ufl.edu", port=1521, service_name="orcl")
-    conn = oracledb.connect(user="hvaradarajan", password="Ftaywlr2E6w4dutANw8t0Lth", params=params)
-    cursor = conn.cursor()
-    cursor.execute('select * from desert')
-    while True:
-        row = cursor.fetchone()
-        if row is None:
-            break
-        print(row)
-    conn.close()
-    return jsonify({"message": "Connection successful, Query5"})
-
-
 if __name__ == '__main__':
     app.run(host='localhost', port=3000)
